37a854c98f01f758dc781181b46591be438e9393
roman <romanokhrimenko@gmail.com>
2020-10-16 15:37:32 +0300

    Add react-todo-app project

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodoListItem from '../todo-list-item/todo-list-item';
+import './todo-list.css';
+
+const TodoList = ({ items, onToggleImportant, onToggleDone, onDelete }) => {
+
+  const listItems = items.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem
+          {...itemProps}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleDone={() => onToggleDone(id)}
+          onDelete={() => onDelete(id)}
+        />
+      </li>
+    );
+  });
+
+  return (<ul className="todo-list list-group">{listItems}</ul>);
+};
+
+export default TodoList;
